ecfd84e39be243bac5b2f966e1f76a8a5a6b9b97
nep <124515520+yenkgris@users.noreply.github.com>
2024-08-27 06:31:15 +0200

    Update irc.py
    comments + placeholders

--- irc.py
+++ irc.py
@@ -4,14 +4,14 @@ from datetime import datetime, timedelta
 import re
 import threading
 
-with open('quiz.json') as file:
+with open('quiz.json') as file: #loads quiz.json with the quizes and their respective answer
     qa_data = json.load(file)
 
 server = 'irc.chat.twitch.tv'
 port = 6667
-nickname = 'essity_'
-token = 'oauth:1h0fn7mlac3e061fo8st5lp00l56ec'
-channel = '#maycris'
+nickname = '{YOURNICKNAME}'
+token = 'oauth:{YOURTOKENHERE}'
+channel = '#{CHANNEL}'
 required_ending = "(You have five minutes to answer correctly, time starts now!)"
 sock = socket.socket()
 sock.connect((server, port))
@@ -19,13 +19,13 @@ sock.send(f"PASS {token}\n".encode('utf-8'))
 sock.send(f"NICK {nickname}\n".encode('utf-8'))
 sock.send(f"JOIN {channel}\n".encode('utf-8'))
 
-def get_question_answa(question):
-    print(f"Searchge: {question}")
+def get_question_answa(question): #searches for the question and gets the answer from quiz.json
+    print(f"Searching: {question}")
     answer = qa_data.get(question.lower())
     if answer:
         print(f"found!!!: {answer}")
     else:
-        print("erm what the sigma")
+        print("something went wrong...")
     return answer or "miyav"
 
 while True:
@@ -41,25 +41,25 @@ while True:
             username = user_info[0].strip()
             message = parts[2].strip()
             print(f"question from {username}: {message}")
-            if "ACTION" in message:
+            if "ACTION" in message: #this is to remove the characters that are added by using /me 
                 oppenheimer = message.split("ACTION", 1)[0]
                 message = message.split("ACTION", 1)[1]
                 message = message.split(oppenheimer, 1)[0]
 
-                if username.lower() == 'PotatBotat'.lower() and "Potato:" in message:
-                    def read_message(message):
+                if username.lower() == 'PotatBotat'.lower() and "Potato:" in message: #this is used to tell at which time will the commands go off cooldowns by using the #status command: message.py
+                    def read_message(message): # pattern only recognizes words such as "Quiz:" and not "Shop-Quiz:" 
                           pattern = r"\b(?<!Shop-)(\w+): (\d+h and \d+m|\d+m and \d+s|\d+s)"
                           matches = re.findall(pattern, message)
                           print(f"Matches found: {matches}")  
                           return matches
                     
-                    def read_message2(message):
+                    def read_message2(message): # pattern only recognizes words such as "Shop-Quiz:" and not "Quiz:" separated due to value lmit
                           pattern2 = r"(\w+-\w+): (\d+h and \d+m|\d+m and \d+s|\d+s)"
                           matches2 = re.findall(pattern2, message)
                           print(f"Shop matches found: {matches2}")
                           return matches2       
                           
-                    def check_cooldown(cooldowns):
+                    def check_cooldown(cooldowns): #calculates at what time of day will any command found by the first pattern be available
                           current_time = datetime.now()
                           availability_times = {}
      
@@ -79,7 +79,7 @@ while True:
                             availability_times[command] = availability_time.strftime("%H:%M:%S")
                           return availability_times
                     
-                    def check_cooldown2(cooldowns2):
+                    def check_cooldown2(cooldowns2): #calculates at what time of day will any command found by the second pattern be available
                           current_time = datetime.now()
                           availability_times2 = {}
      
@@ -103,19 +103,19 @@ while True:
                     cooldowns2 = read_message2(message)
                     availability_times = check_cooldown(cooldowns)
                     availability_times2 = check_cooldown2(cooldowns2)
-                    for command, time in availability_times.items():
+                    for command, time in availability_times.items(): #displays on the terminal the final result using the first pattern
                       print(f"{command} will be available at {time}")
 
-                    for command2, time in availability_times2.items():
+                    for command2, time in availability_times2.items(): #displays on the terminal the final result using the second pattern
                      print(f"{command2} will be available at {time}")   
 
-                    if required_ending in message:
-                        def delay_message():
+                    if required_ending in message: #this is done so it only searches for the quiz answer if the sentence sent by the bot ends with the required ending
+                        def delay_message(): #to add a delay between the quiz command being triggered and the answer sent using threading.Timer
                             sock.send(f"PRIVMSG {channel} :{final_answer}\n".encode('utf-8'))
 
                         print(message)
-                        message = message.replace(" @フェムシャーク ", "").lower()
-                        print(f"PAAAAAUSE: {message}")
+                        message = message.replace(" @{YOURUSER} ", "").lower() #removes the username to correctly read from quiz.json
+                        print(f"final message{message}")
 
                         answer = get_question_answa(message)
                         final_answer = (f"#a {answer}")
@@ -123,476 +123,3 @@ while True:
                         timer.start()
 
                     
-
-                
-
-
-
-               
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# if erm == True
-#   print("meow")                  
